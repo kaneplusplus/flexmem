0f97d1166727a0a92ee5688d2a93672577b20e2d
kaneplusplus <kaneplusplus@gmail.com>
2015-12-30 13:04:53 -0500

    Swap benchmarks.

--- benchmarks/R/debian/xmem-vector-random-access.r
+++ benchmarks/R/debian/xmem-vector-random-access.r
@@ -14,19 +14,21 @@ bench = rep(NA, length(sizes))
 print("read")
 # Random access for a vector 
 for (i in 1:length(sizes)) {
-  print(i)
-  v = vector(mode="numeric", length=sizes[i])
-  object_size[i] = format(object.size(v), "Gb")
-  print(object.size(v), units="Gb")
-  bench[i] = summary(microbenchmark(
-    {
-      inds = sample.int(length(v), floor(sizes[i]*prop), replace=TRUE)
-      v[inds]
-    }, times=1, unit="s"))$mean
-  print("Calling gc")
-  gc()
-  print(object_size[i])
-  print(bench[i])
+  try({
+    print(i)
+    v = vector(mode="numeric", length=sizes[i])
+    object_size[i] = format(object.size(v), "Gb")
+    print(object.size(v), units="Gb")
+    bench[i] = summary(microbenchmark(
+      {
+        inds = sample.int(length(v), floor(sizes[i]*prop), replace=TRUE)
+        v[inds]
+      }, times=1, unit="s"))$mean
+    print("Calling gc")
+    gc()
+    print(object_size[i])
+    print(bench[i])
+  })
 }
 
 df = data.frame(object_size=object_size, seconds=bench, memory="xmem",
@@ -35,18 +37,20 @@ df = data.frame(object_size=object_size, seconds=bench, memory="xmem",
 print("write")
 # random write. 
 for (i in 1:length(sizes)) {
-  print(i)
-  v = vector(mode="numeric", length=sizes[i])
-  object_size[i] = format(object.size(v), "Gb")
-  bench[i] = summary(microbenchmark(
-    {
-      inds = sample.int(length(v), floor(sizes[i]*prop), replace=TRUE)
-      values = as.double(sample(1:10, length(inds), replace=TRUE))
-      v[inds] = values
-    }, times=1, unit="s"))$median
-  gc()
-  print(object_size[i])
-  print(bench[i])
+  try({
+    print(i)
+    v = vector(mode="numeric", length=sizes[i])
+    object_size[i] = format(object.size(v), "Gb")
+    bench[i] = summary(microbenchmark(
+      {
+        inds = sample.int(length(v), floor(sizes[i]*prop), replace=TRUE)
+        values = as.double(sample(1:10, length(inds), replace=TRUE))
+        v[inds] = values
+      }, times=1, unit="s"))$median
+    gc()
+    print(object_size[i])
+    print(bench[i])
+  })
 }
 
 df = cbind(df,
